000582a8dd2afe40aff1c70bd6432a2254c74d8b
Piyush Srivastava <piyush@thesileo.com>
2024-11-15 16:10:59 +0530

    Made an API to save student's basic details.

--- src/main/java/com/example/student_management_system/repositories/StudentRepository.java
+++ src/main/java/com/example/student_management_system/repositories/StudentRepository.java
@@ -1,4 +1,9 @@
 package com.example.student_management_system.repositories;
 
-public interface StudentRepository {
+import com.example.student_management_system.entities.StudentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<StudentEntity, Long> {
 }
