7983f2871e5e162ae392bfab1152d59cad7e8676
Piyush Srivastava <piyush@thesileo.com>
2024-11-15 16:04:30 +0530

    Built core structure of project.

---
+++ src/main/java/com/example/student_management_system/models/StudentModel.java
@@ -0,0 +1,40 @@
+package com.example.student_management_system.models;
+
+public class StudentModel {
+    private Long studentId;
+    private String studentName;
+    private String email;
+    private String phone;
+
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ src/main/java/com/example/student_management_system/repositories/StudentRepository.java
@@ -0,0 +1,4 @@
+package com.example.student_management_system.repositories;
+
+public interface StudentRepository {
+}
